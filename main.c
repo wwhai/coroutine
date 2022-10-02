41cde0c600240537447e1673768699061950bb4d
wwhai <cnwwhai@gmail.com>
2022-10-03 00:48:11 +0800

    dev: modify some code

--- main.c
+++ main.c
@@ -1,42 +1,43 @@
 #include "coroutine.h"
 #include <stdio.h>
-
-struct args {
+#include <unistd.h>
+struct args
+{
 	int n;
 };
 
-static void
-foo(struct schedule * S, void *ud) {
-	struct args * arg = ud;
-	int start = arg->n;
-	int i;
-	for (i=0;i<5;i++) {
-		printf("coroutine %d : %d\n",coroutine_running(S) , start + i);
+static void foo1(struct schedule *S, void *args)
+{
+	while (1)
+	{
+		sleep(1);
+		printf("1\n");
+		coroutine_yield(S);
+	}
+}
+static void foo2(struct schedule *S, void *args)
+{
+	while (1)
+	{
+		sleep(2);
+		printf("2\n");
 		coroutine_yield(S);
 	}
 }
 
-static void
-test(struct schedule *S) {
-	struct args arg1 = { 0 };
-	struct args arg2 = { 100 };
 
-	int co1 = coroutine_new(S, foo, &arg1);
-	int co2 = coroutine_new(S, foo, &arg2);
-	printf("main start\n");
-	while (coroutine_status(S,co1) && coroutine_status(S,co2)) {
-		coroutine_resume(S,co1);
-		coroutine_resume(S,co2);
-	} 
-	printf("main end\n");
-}
-
-int 
-main() {
-	struct schedule * S = coroutine_open();
-	test(S);
+int main()
+{
+	struct schedule *S = coroutine_open();
+	struct args arg1 = {1};
+	struct args arg2 = {2};
+	int co1 = coroutine_new(S, foo1, &arg1);
+	int co2 = coroutine_new(S, foo2, &arg2);
+	while (coroutine_status(S, co1) && coroutine_status(S, co2))
+	{
+		coroutine_resume(S, co1);
+		coroutine_resume(S, co2);
+	}
 	coroutine_close(S);
-	
 	return 0;
 }
-
